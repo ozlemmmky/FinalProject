3811617cb08ab48d1fea00ce0bb5bb110fd649ae
ozlemmmky <ozlemk.1667@gmail.com>
2023-06-21 10:49:45 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using ConsoleUI;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class ConsoleUI
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProducDal());
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+        }
+    }
+}
+
