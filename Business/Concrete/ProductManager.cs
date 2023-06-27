88588d1ed66763bfa22a7f30e30e096040daacde
ozlemmmky <ozlemk.1667@gmail.com>
2023-06-27 14:37:07 +0300

    Validation

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,16 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,14 +24,11 @@ namespace Business.Concrete
         {
             _ProductDal = productDal;
         }
-
+        [ValidationAspect(typeof (ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-            //business codes
+            
+           ValidationTool.Validate(new ProductValidator(),product);
             _ProductDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.Data.SqlClient;
@@ -13,10 +16,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //Autofac,ninject,castlewindsor-->Ioc Container
-builder.Services.AddSingleton<IProductService,ProductManager>();//Iproductservice görürsen productmanager'ý newle
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+//builder.Services.AddSingleton<IProductService,ProductManager>();//Iproductservice görürsen productmanager'ý newle
+//builder.Services.AddSingleton<IProductDal,EfProductDal>();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 //data tutmuyorsak singleeton kullanabiliriz.
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
